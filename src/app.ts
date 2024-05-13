16c65ec606b226fe2fe1d78a120538ad3385dbaf
neko <ai73a@yandex.ru>
2024-05-13 12:54:34 +0300

    block-3

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import cors from 'cors'
+import {getVideosController} from './videos/getVideosController'
+import {SETTINGS} from './settings'
 
 export const app = express() // создать приложение
 app.use(express.json()) // создание свойств-объектов body и query во всех реквестах
@@ -9,5 +11,5 @@ app.get('/', (req, res) => {
     // эндпоинт, который будет показывать на верселе какая версия бэкэнда сейчас залита
     res.status(200).json({version: '1.0'})
 })
-// app.get(SETTINGS.PATH.VIDEOS, getVideosController)
+app.get(SETTINGS.PATH.VIDEOS, getVideosController)
 // app.use(SETTINGS.PATH.VIDEOS, videosRouter)
